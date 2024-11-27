d6bd185e17e08164a7785ecb79460bf7da881116
Aldivan Vieira De Souza <aldvieir@c3r2p5.42sp.org.br>
2024-11-27 16:34:36 -0300

    2

---
+++ get_next_line/get_next_line.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aldvieir <aldvieir@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/25 15:10:34 by aldvieir          #+#    #+#             */
+/*   Updated: 2024/11/25 16:53:49 by aldvieir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+char *get_next_line(int fd)
+{
+	ssize_t read(int fildes, void *buf, size_t nbyte);
+
+	if (!)
+		return (NULL);
+	
+}
