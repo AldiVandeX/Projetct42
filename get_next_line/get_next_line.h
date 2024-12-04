b5148a43d39384942bab80672d489d87ef2231c3
Aldivan Vieira De Souza <aldvieir@c3r5p3.42sp.org.br>
2024-12-04 15:37:05 -0300

    a

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -3,23 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aldvieir <aldvieir@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jreis-do <jreis-do@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/25 16:26:55 by aldvieir          #+#    #+#             */
-/*   Updated: 2024/12/04 12:16:36 by aldvieir         ###   ########.fr       */
+/*   Created: 2024/10/31 16:45:18 by jreis-do          #+#    #+#             */
+/*   Updated: 2024/11/14 08:12:03 by jreis-do         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFFER_SIZE 42
 
-# include <unistd.h>
-# include <stdio.h>
 # include <stdlib.h>
-# include <fcntl.h>
+# include <unistd.h>
 
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 
-char *get_next_line(int fd);
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
+char	*get_next_line(int fd);
 
 #endif
